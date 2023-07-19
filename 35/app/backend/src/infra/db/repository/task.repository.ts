fc87d66775e05ee417502da006ee63f7e1b5fa27
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-07-19 20:17:56 +0900

    タスクを追加できるようにする

---
+++ 35/app/backend/src/infra/db/repository/task.repository.ts
@@ -0,0 +1,8 @@
+import { Task } from 'src/domain/task'
+import { TaskRepositoryInterface } from 'src/domain/task.repository.interface'
+
+export class TaskRepository implements TaskRepositoryInterface {
+  async add(task: Task) {
+    return
+  }
+}
