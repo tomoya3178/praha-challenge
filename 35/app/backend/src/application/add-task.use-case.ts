156890b59d20a27879c97bcea61e445b1209e1ff
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-09-29 21:37:13 +0900

    fix

--- 35/app/backend/src/application/add-task.use-case.ts
+++ 35/app/backend/src/application/add-task.use-case.ts
@@ -1,9 +1,9 @@
-import { Task, TaskInterface } from 'src/domain/task';
+import { Task } from 'src/domain/task';
 import { TaskRepositoryInterface } from 'src/domain/task.repository.interface';
 
 export class AddTaskUseCase {
   constructor(private readonly repository: TaskRepositoryInterface) {}
-  async execute(input: TaskInterface) {
+  async execute(input: Task['value']) {
     const task = new Task(input);
     await this.repository.add(task);
   }
