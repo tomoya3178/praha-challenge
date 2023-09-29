69cb05bd8e4d47314436e936db590beec2e070ac
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-09-29 21:27:58 +0900

    fix

---
+++ 35/app/backend/prisma/migrations/20230929122720_/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Tasks" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Tasks_pkey" PRIMARY KEY ("id")
+);
