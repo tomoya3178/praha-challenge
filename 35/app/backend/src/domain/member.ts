5fdb2423feda3eb9c76b196af667bd0a8e3b9bb2
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-09 20:56:32 +0900

    fix

--- 35/app/backend/src/domain/member.ts
+++ 35/app/backend/src/domain/member.ts
@@ -1,4 +1,25 @@
 import { Id } from './id';
+import { Task } from './task';
+
+export class AssignedTask {
+  readonly value: {
+    readonly id: Id;
+    readonly taskId: Task['value']['id'];
+    readonly status: '未着手' | 'レビュー待ち' | '完了';
+  };
+  constructor(input: AssignedTask['value']) {
+    this.value = input;
+  }
+  changeStatus(status: AssignedTask['value']['status']) {
+    if (this.value.status === '完了') {
+      throw new Error();
+    }
+    return new AssignedTask({
+      ...this.value,
+      status,
+    });
+  }
+}
 
 export class Member {
   readonly value: {
@@ -6,9 +27,33 @@ export class Member {
     readonly status: '在籍中' | '休会中' | '退会済';
     readonly name: string;
     readonly email: string;
+    readonly assignedTasks: AssignedTask[];
   };
   constructor(input: Member['value']) {
     this.value = input;
+  }
+  assignTasks(tasks: Task[]) {
+    if (
+      this.value.assignedTasks.some((assignedTask) =>
+        tasks.some((task) => assignedTask.value.taskId.equals(task.value.id)),
+      )
+    ) {
+      throw new Error();
+    }
+    return new Member({
+      ...this.value,
+      assignedTasks: [
+        ...this.value.assignedTasks,
+        ...tasks.map(
+          (task) =>
+            new AssignedTask({
+              id: Id.init(),
+              taskId: task.value.id,
+              status: '未着手',
+            }),
+        ),
+      ],
+    });
   }
   toObject() {
     const { id, ...rest } = this.value;
