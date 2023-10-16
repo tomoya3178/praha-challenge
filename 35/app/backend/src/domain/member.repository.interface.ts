a290a4657c6daaaefc4afa1be1b2499c09564626
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-16 21:40:48 +0900

    fix

--- 35/app/backend/src/domain/member.repository.interface.ts
+++ 35/app/backend/src/domain/member.repository.interface.ts
@@ -2,5 +2,5 @@ import { Member } from './member';
 
 export interface MemberRepositoryInterface {
   add(member: Member): Promise<void>;
-  findByEmail(email: Member['value']['email']): Promise<Member>;
+  findByEmail(email: Member['value']['email']): Promise<Member | undefined>;
 }
