30fb7fe825ae622e71d166733dbda24e4c568971
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-16 22:00:21 +0900

    fix

--- 35/app/backend/src/application/add-task.use-case.ts
+++ 35/app/backend/src/application/add-task.use-case.ts
@@ -1,8 +1,15 @@
+import { Inject } from '@nestjs/common';
 import { Task } from 'src/domain/task';
-import { TaskRepositoryInterface } from 'src/domain/task.repository.interface';
+import {
+  TASK_REPOSITORY_TOKEN,
+  TaskRepositoryInterface,
+} from 'src/domain/task.repository.interface';
 
 export class AddTaskUseCase {
-  constructor(private readonly repository: TaskRepositoryInterface) {}
+  constructor(
+    @Inject(TASK_REPOSITORY_TOKEN)
+    private readonly repository: TaskRepositoryInterface,
+  ) {}
   async execute(input: Task['value']) {
     const task = new Task(input);
     await this.repository.add(task);
