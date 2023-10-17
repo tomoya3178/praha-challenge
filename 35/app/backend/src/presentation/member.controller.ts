a65a7326758bbe52bc3b2573d7de8dcee18172dc
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-17 11:31:13 +0900

    fix

--- 35/app/backend/src/presentation/member.controller.ts
+++ 35/app/backend/src/presentation/member.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AddMemberUseCase } from 'src/application/add-member.use-case';
 import { Id } from 'src/domain/id';
-import { Member } from 'src/domain/member';
+import { AssignedTask, Member } from 'src/domain/member';
 
 @Controller('members')
 export class MemberController {
@@ -13,11 +13,24 @@ export class MemberController {
       id: Member['value']['id']['value'];
       name: Member['value']['name'];
       email: Member['value']['email'];
+      assignedTasks: {
+        id: Member['value']['assignedTasks'][number]['value']['id']['value'];
+        taskId: Member['value']['assignedTasks'][number]['value']['taskId']['value'];
+        status: Member['value']['assignedTasks'][number]['value']['status'];
+      }[];
     },
   ): Promise<void> {
     await this.addMemberUseCase.execute({
       ...request,
       id: new Id(request.id),
+      assignedTasks: request.assignedTasks.map(
+        (assignedTask) =>
+          new AssignedTask({
+            ...assignedTask,
+            id: new Id(assignedTask.id),
+            taskId: new Id(assignedTask.taskId),
+          }),
+      ),
     });
   }
 }
