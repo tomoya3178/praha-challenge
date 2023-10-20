de3649ae0295c783cfdc9c404aa6f1be641a433c
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-20 18:32:23 +0900

    fix

--- 35/app/backend/src/domain/member.repository.interface.ts
+++ 35/app/backend/src/domain/member.repository.interface.ts
@@ -3,4 +3,6 @@ import { Member } from './member';
 export interface MemberRepositoryInterface {
   add(member: Member): Promise<void>;
   findByEmail(email: Member['value']['email']): Promise<Member | undefined>;
+  findById(id: Member['value']['id']): Promise<Member | undefined>;
+  update(member: Member): Promise<void>;
 }
--- 35/app/backend/src/domain/task.repository.interface.ts
+++ 35/app/backend/src/domain/task.repository.interface.ts
@@ -2,4 +2,5 @@ import { Task } from './task';
 
 export interface TaskRepositoryInterface {
   add(task: Task): Promise<void>;
+  findByIds(ids: Task['value']['id'][]): Promise<Task[]>;
 }
