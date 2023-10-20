92308ad8307c34732660758b88973afb5bb2cf22
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-20 18:46:23 +0900

    fix

--- 35/app/backend/src/infrastructure/task.repository.ts
+++ 35/app/backend/src/infrastructure/task.repository.ts
@@ -1,6 +1,7 @@
 import { Task } from 'src/domain/task';
 import { TaskRepositoryInterface } from 'src/domain/task.repository.interface';
 import { PrismaService } from './prisma.service';
+import { Id } from 'src/domain/id';
 
 export class TaskRepository implements TaskRepositoryInterface {
   constructor(private readonly prisma: PrismaService) {}
@@ -8,5 +9,17 @@ export class TaskRepository implements TaskRepositoryInterface {
     await this.prisma.task.create({
       data: task.toObject(),
     });
+  }
+  async findByIds(ids: Id[]): Promise<Task[]> {
+    const rows = await this.prisma.task.findMany({
+      where: { id: { in: ids.map((id) => id.toString()) } },
+    });
+    return rows.map(
+      (row) =>
+        new Task({
+          ...row,
+          id: new Id(row.id),
+        }),
+    );
   }
 }
