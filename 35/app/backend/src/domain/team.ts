0c36ee9d5caf339f3b35706a397b817e5d16e463
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-22 16:47:06 +0900

    fix

---
+++ 35/app/backend/src/domain/team.ts
@@ -0,0 +1,95 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { Member } from './member';
+import { Pair, Name as PairName } from './pair';
+import { Id } from './id';
+
+export class Name {
+  readonly value: number;
+  constructor(input: Name['value']) {
+    if (String(input).length > 3) {
+      throw new InternalServerErrorException();
+    }
+    this.value = input;
+  }
+}
+
+export class Team {
+  readonly value: {
+    readonly id: Id;
+    readonly name: Name;
+    readonly pairs: Pair[];
+  };
+  constructor(input: Team['value']) {
+    this.value = input;
+  }
+  addMember(member: Member) {
+    const smallestPair = this.value.pairs.reduce((smallest, current) => {
+      if (smallest.value.members.length <= current.value.members.length) {
+        return smallest;
+      }
+      return current;
+    });
+    if (smallestPair.value.members.length === 3) {
+      const movingMember = smallestPair.value.members.at(-1);
+      if (!movingMember) {
+        throw new InternalServerErrorException();
+      }
+      return new Team({
+        ...this.value,
+        pairs: [
+          ...this.value.pairs.map((x) => {
+            if (x.value.id.equals(smallestPair.value.id)) {
+              return x.removeMember(movingMember);
+            }
+            return x;
+          }),
+          new Pair({
+            id: Id.init(),
+            name: new PairName('z'),
+            members: [movingMember, member],
+          }),
+        ],
+      });
+    }
+    return new Team({
+      ...this.value,
+      pairs: this.value.pairs.map((x) => {
+        if (x.value.id.equals(smallestPair.value.id)) {
+          return x.addMember(member);
+        }
+        return x;
+      }),
+    });
+  }
+  removeMember(member: Member) {
+    const pair = this.value.pairs.find((pair) =>
+      pair.value.members.some((x) => x.value.id.equals(member.value.id)),
+    );
+    if (!pair) {
+      throw new InternalServerErrorException();
+    }
+    if (pair.value.members.length === 2) {
+      const restMember = pair.value.members.find(
+        (x) => !x.value.id.equals(member.value.id),
+      );
+      if (!restMember) {
+        throw new InternalServerErrorException();
+      }
+      return new Team({
+        ...this.value,
+        pairs: this.value.pairs.filter(
+          (x) => !x.value.id.equals(pair.value.id),
+        ),
+      }).addMember(restMember);
+    }
+    return new Team({
+      ...this.value,
+      pairs: this.value.pairs.map((x) => {
+        if (x.value.id.equals(pair.value.id)) {
+          return x.removeMember(member);
+        }
+        return x;
+      }),
+    });
+  }
+}
