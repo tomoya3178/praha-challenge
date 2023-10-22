8f651dcbd24f27694df28a14c6017544468fca9d
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-22 18:28:38 +0900

    fix

--- 35/app/backend/src/application/add-member.use-case.ts
+++ 35/app/backend/src/application/add-member.use-case.ts
@@ -1,25 +1,32 @@
 import { BadRequestException, Inject } from '@nestjs/common';
 import { Member } from 'src/domain/member';
-import {
-  MEMBER_REPOSITORY_TOKEN,
-  MemberRepositoryInterface,
-} from 'src/domain/member.repository.interface';
 import { MemberService } from 'src/domain/member.service';
+import { Team } from 'src/domain/team';
+import {
+  TEAM_REPOSITORY_TOKEN,
+  TeamRepositoryInterface,
+} from 'src/domain/team.repository.interface';
 
 export class AddMemberUseCase {
   constructor(
     private readonly service: MemberService,
-    @Inject(MEMBER_REPOSITORY_TOKEN)
-    private readonly repository: MemberRepositoryInterface,
+    @Inject(TEAM_REPOSITORY_TOKEN)
+    private readonly repository: TeamRepositoryInterface,
   ) {}
-  async execute(input: Omit<Member['value'], 'status'>) {
+  async execute(
+    id: Team['value']['id'],
+    input: Omit<Member['value'], 'status'>,
+  ) {
     if (await this.service.emailExists(input.email)) {
       throw new BadRequestException();
     }
-    const member = new Member({
-      ...input,
-      status: 'ACTIVE',
-    });
-    await this.repository.add(member);
+    const team = await this.repository.findById(id);
+    const updatedTeam = team.addMember(
+      new Member({
+        ...input,
+        status: 'ACTIVE',
+      }),
+    );
+    await this.repository.update(updatedTeam);
   }
 }
