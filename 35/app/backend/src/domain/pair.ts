e162cd91f3f1805bf4618ee2172851c4f6c622b5
tomoya3178 <60164103+tomoya3178@users.noreply.github.com>
2023-10-22 16:47:06 +0900

    fix

---
+++ 35/app/backend/src/domain/pair.ts
@@ -0,0 +1,44 @@
+import { InternalServerErrorException } from '@nestjs/common';
+import { Member } from './member';
+import { Id } from './id';
+
+export class Name {
+  readonly value: string;
+  constructor(input: Name['value']) {
+    if (!/[a-z]/.test(input)) {
+      throw new InternalServerErrorException();
+    }
+    this.value = input;
+  }
+}
+
+export class Pair {
+  readonly value: {
+    readonly id: Id;
+    readonly name: Name;
+    readonly members: Member[];
+  };
+  constructor(input: Pair['value']) {
+    this.value = input;
+  }
+  addMember(member: Member) {
+    if (this.value.members.length >= 3) {
+      throw new InternalServerErrorException();
+    }
+    return new Pair({
+      ...this.value,
+      members: [...this.value.members, member],
+    });
+  }
+  removeMember(member: Member) {
+    if (this.value.members.length <= 2) {
+      throw new InternalServerErrorException();
+    }
+    return new Pair({
+      ...this.value,
+      members: this.value.members.filter(
+        (x) => !x.value.id.equals(member.value.id),
+      ),
+    });
+  }
+}
